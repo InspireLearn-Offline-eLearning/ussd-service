f6276d5692bbb4393771edb9ba04517cef457f9e
Masikini <sammasikini1@gmail.com>
2024-08-20 14:35:29 +0200

    fixed conference time validation

--- app/Screens/Schedule_Conf_Time.php
+++ app/Screens/Schedule_Conf_Time.php
@@ -5,12 +5,12 @@ namespace App\Screens;
 
 
 use TNM\USSD\Screen;
-use TNM\USSD\Http\Validates;
+// use TNM\USSD\Http\Validates;
 use TNM\USSD\Exceptions\UssdException;
 
 class Schedule_Conf_Time extends Screen
 {
-    use Validates;
+    // use Validates;
 
     protected function message(): string
     {
@@ -29,32 +29,42 @@ class Schedule_Conf_Time extends Screen
 
     protected function execute(): mixed
     {
-        $this->validate($this->request, 'time');
+        // $this->validate($this->request, 'time');
         $time = $this->validateTimeInput($this->value());
         if ($time) {
             $this->addPayload('conf_time', $time);
             return (new Schedule_Conf_Confirm($this->request))->render();
         }
 
-        throw new UssdException($this->request, "Something went wrong, Please try again later");
+        throw new UssdException($this->request, "Incorrect time,please try again later");
     }
 
 
-    protected function rules(): string
-    {
+    // protected function rules(): string
+    // {
 
-        return 'regex:/^\d{4}$/';
-    }
+    //     return 'regex:/^\d{4}$/';
+    // }
 
     function validateTimeInput($input)
     {
-
+        date_default_timezone_set('Africa/Blantyre'); 
 
         $hours = substr($input, 0, 2);
         $minutes = substr($input, 2, 2);
 
         if ($hours >= 0 && $hours <= 23 && $minutes >= 0 && $minutes <= 59) {
-            return $hours . ':' . $minutes;
+           
+            $inputTime =  $hours . ':' . $minutes;
+
+            \Log::debug('Input time is:', ['value' => $inputTime]);
+            $currentTime = date('H:i');
+            \Log::debug('Current time is:', ['value' => $currentTime]);
+            if ($inputTime < $currentTime) {
+                return false; // Time is in the past
+            }
+
+            return $inputTime; 
         }
 
         return false; // Invalid time format
